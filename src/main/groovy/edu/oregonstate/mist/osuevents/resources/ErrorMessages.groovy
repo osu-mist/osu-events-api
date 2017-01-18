aac46a4693eb923eeb045d5166bac0e1836c9774
Jared Kosanovic <jared@Jareds-MacBook-Pro.local>
2017-01-18 08:19:08 -0800

    add class for common error messages

---
+++ src/main/groovy/edu/oregonstate/mist/osuevents/resources/ErrorMessages.groovy
@@ -0,0 +1,9 @@
+package edu.oregonstate.mist.osuevents.resources
+
+class ErrorMessages {
+    public final static String unknownFields = "Event contains unrecognized fields."
+
+    public final static String parseDate = "Unable to parse date. Dates should follow ISO 8601 specifications."
+
+    public final static String processInstance = "Unable to process instance. Ensure instance ID is a string."
+}
