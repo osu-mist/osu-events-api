7af9c0d7896b5bea8252112f66d69243fc96a1d8
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-05-23 14:49:51 -0400

    remove imports
    unit tests will be added later

--- src/test/groovy/edu/oregonstate/mist/osuevents/EventsResourceTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/osuevents/EventsResourceTest.groovy
@@ -1,24 +1,5 @@
 package edu.oregonstate.mist.osuevents
 
-import edu.oregonstate.mist.osuevents.resources.CSVHelperFunctions
-import edu.oregonstate.mist.api.AuthenticatedUser
-import edu.oregonstate.mist.api.jsonapi.ResourceObject
-import edu.oregonstate.mist.api.jsonapi.ResultObject
-import edu.oregonstate.mist.osuevents.core.Event
-import edu.oregonstate.mist.osuevents.core.Instance
-import edu.oregonstate.mist.osuevents.db.EventsDAO
-import edu.oregonstate.mist.osuevents.resources.ErrorMessages
-import edu.oregonstate.mist.osuevents.resources.EventsResource
-import groovy.mock.interceptor.MockFor
-import io.dropwizard.testing.junit.DropwizardAppRule
-import org.junit.ClassRule
-import org.junit.Test
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-
-import static java.util.UUID.randomUUID
-
 class EventsResourceTest {
     //TODO: add unit tests
 }
