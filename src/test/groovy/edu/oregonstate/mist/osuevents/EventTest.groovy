6d90fffe2944dacb79caad1a76c44a69f550e693
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-05-24 15:02:21 -0400

    add getByID unit test
    Going to have to add a lot more tests but I realized this is probably best done after making sure the API is working and validating as expected.

---
+++ src/test/groovy/edu/oregonstate/mist/osuevents/EventTest.groovy
@@ -0,0 +1,52 @@
+package edu.oregonstate.mist.osuevents
+
+import edu.oregonstate.mist.osuevents.core.Event
+import edu.oregonstate.mist.osuevents.core.Instance
+
+import java.time.ZonedDateTime
+
+class EventTest {
+    public static Event validSampleEvent = new Event(
+            eventID: "some-nasty-uuid-here",
+            title: "Unit Testing",
+            description: "Do unit testing.",
+            locationID: "1337",
+            otherLocationName: null, //if a location ID is used, this field should be null
+            room: "Down the hall and to the left.",
+            address: null, //if a location ID is used, this field should be null
+            city: null, //if a location ID is used, this field should be null
+            state: null, //if a location ID is used, this field should be null
+            countyID: "Benton", //this technically doesn't need to be null if a locationID is given
+            eventURL: "https://example.com/foo",
+            photoURL: "https://example.com/foo.jpg",
+            facebookURL: "https://facebook.com/some-event-page",
+            ticketURL: "https://someticketplace.com/foo",
+            ticketCost: "This can be any string.",
+            hashtag: "foo",
+            keywords: ["foo", "bar", "eggplant"],
+            tags: ["unit-testing", "testing"],
+            allowsReviews: true,
+            sponsored: false,
+            venuePageOnly: true,
+            excludeFromTrending: false,
+            allowUserActivity: true,
+            allowUserInterest: false,
+            departmentIDs: ["1", "2", "3"],
+            contactName: "Jared",
+            contactEmail: "jared@foo.com",
+            contactPhone: "555-555-5555",
+            eventTypeIDs: ["2", "1"],
+            eventTopicIDs: ["2", "3"],
+            audienceIDs: ["3", "4"],
+            instances: [
+                    new Instance(
+                            start: ZonedDateTime.now(),
+                            end: ZonedDateTime.now().plusHours(1)
+                    ),
+                    new Instance(
+                            start: ZonedDateTime.now().plusDays(1),
+                            end: null
+                    )
+            ]
+    )
+}
--- src/test/groovy/edu/oregonstate/mist/osuevents/EventsResourceTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/osuevents/EventsResourceTest.groovy
@@ -1,5 +1,38 @@
 package edu.oregonstate.mist.osuevents
 
+import edu.oregonstate.mist.api.Error
+import edu.oregonstate.mist.osuevents.core.Event
+import edu.oregonstate.mist.osuevents.db.EventsDAOWrapper
+import edu.oregonstate.mist.osuevents.resources.EventsResource
+import groovy.mock.interceptor.MockFor
+import org.junit.Test
+
+import javax.ws.rs.core.Response
+
+import static org.junit.Assert.assertEquals
+
 class EventsResourceTest {
-    //TODO: add unit tests
+
+    @Test
+    void testGetEventByID() {
+        def notFoundEventsDAOWrapper = new MockFor(EventsDAOWrapper)
+        notFoundEventsDAOWrapper.demand.getEventByID() { String eventID -> null }
+
+        EventsResource eventsResource = new EventsResource(notFoundEventsDAOWrapper.proxyInstance())
+
+        Response notFoundResponse = eventsResource.getEventByID("something")
+        assertEquals(404, notFoundResponse.status)
+        assertEquals(Error.class, notFoundResponse.entity.class)
+
+        def foundEventsDAOWrapper = new MockFor(EventsDAOWrapper)
+        foundEventsDAOWrapper.demand.getEventByID() { String eventID -> EventTest.validSampleEvent }
+        eventsResource = new EventsResource(foundEventsDAOWrapper.proxyInstance())
+
+        Response goodResponse = eventsResource.getEventByID("something")
+        assertEquals(200, goodResponse.status)
+
+        def event = goodResponse.entity["data"]["attributes"]
+        assertEquals(Event.class, event.class)
+        assertEquals(EventTest.validSampleEvent, event)
+    }
 }
